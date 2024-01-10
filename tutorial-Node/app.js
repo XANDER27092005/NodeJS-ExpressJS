a83ce109c97c4873e9eac3bbfd75e2602e81f3b8
XANDER27092005 <michael.iloba@pau.edu.ng>
2024-01-10 06:59:27 +0000

    node

--- tutorial-Node/app.js
+++ tutorial-Node/app.js
@@ -1,9 +1,19 @@
-const john = "John";
-const peter = "Peter";
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`)
+const { log } = require('console');
+const os = require('os');
+
+// info about current user
+const user = os.userInfo();
+console.log(user);
+
+
+// method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
 }
 
-sayHi("susan");
-sayHi(john);
-sayHi(peter);
+console.log(currentOS);
